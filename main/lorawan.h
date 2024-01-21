74f7d40b18e82727faa28684b48f8f4146f28c83
Max Hunter <nippoo@nippoo.net>
2024-01-21 12:54:20 +0000

    Removed extraneous LoRaWAN code

--- main/lorawan.h
+++ main/lorawan.h
@@ -7,11 +7,6 @@ extern "C" {
 #endif
 
 /**
- * @brief FreeRTOS CAN bus receive task
- */
-void can_receive_task(void *arg);
-
-/**
  * @brief Initialize LoRaWAN and create telemetry task
  */
 esp_err_t lorawan_init();
